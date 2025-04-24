9e41319e9d747f9af61193d27e51be3420570fd7
KarasuZero <60535595+KarasuZero@users.noreply.github.com>
2025-04-24 04:06:40 -0500

    Layer and Navi Update
    separated water from land layer, added navi element to land layer, Navi agend2d added to player, as well as click to navigate

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,48 +1,41 @@
 extends CharacterBody2D
 
-# player animated sprite node
-@onready var player_anim = $AnimatedSprite2D
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
 
-# global variables 
-const SPEED = 100.0
-# current player direction, default to front/idle
-var current_dir = "front"
-var current_anim = "idle_front"
+var movement_speed = 25.0
 
-func _ready():
-	player_anim.play("idle_front")
+func _input(event):
+	if event.is_action_pressed("Click_To_Move"):
+		var mouse_position = get_global_mouse_position()
+		
+		# get navi map from agent
+		var nav_map = navigation_agent_2d.get_navigation_map()
+		
+		#get closest point valid for naigation
+		var closest_point = NavigationServer2D.map_get_closest_point(nav_map,mouse_position)
+		
+		if closest_point != null:
+			navigation_agent_2d.target_position = closest_point
+			print("input is working")
 
-func _physics_process(delta):
-	player_movement(delta)
+
+func _physics_process(delta: float) -> void:
+
 	
-func player_movement(delta):
-	velocity.x = 0
-	velocity.y = 0
+	var current_agent_position = global_position
+	var next_path_position = navigation_agent_2d.get_next_path_position()
+	var new_velocity = current_agent_position.direction_to(next_path_position) * movement_speed
 	
-	#movement control, is aciton pressed for continues press, is event for on time press 
-	if Input.is_action_pressed("ui_right"):
-		player_anim.flip_h = false
-		current_dir = "side"
-		velocity.x = SPEED
-		
-	elif Input.is_action_pressed("ui_left"):
-		player_anim.flip_h = true
-		current_dir = "side"
-		velocity.x = -SPEED
-		
-	elif Input.is_action_pressed("ui_down"):
-		current_dir = "front"
-		velocity.y = SPEED
-		
-	elif Input.is_action_pressed("ui_up"):
-		current_dir = "back"
-		velocity.y = -SPEED
+	# navigation has ended
+	if navigation_agent_2d.is_navigation_finished():
+		return
 	
-	# Set animation based on movement - keeps last direction
-	if velocity.length() > 0:
-		current_anim = "walk_" + current_dir
+	if navigation_agent_2d.avoidance_enabled:
+		navigation_agent_2d.set_velocity(new_velocity)
 	else:
-		current_anim = "idle_" + current_dir
+		_on_navigation_agent_2d_velocity_computed(new_velocity)
 	
-	player_anim.play(current_anim)
 	move_and_slide()
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
